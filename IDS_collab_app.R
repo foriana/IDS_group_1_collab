775a7cee7b812126f5e7d4d8bd499c2569160e5b
foriana <joyf.peterson@gmail.com>
2022-04-21 08:16:08 -0500

    Commit beginning shiny app and Felicia markdown

--- IDS_collab_app.R
+++ IDS_collab_app.R
@@ -1,14 +1,24 @@
 library(shiny)
-library(lubridate) 
+library(shinythemes) 
 library(RColorBrewer)
-library(plotly)   
-library(ggthemes) 
-ui <- fluidPage()
-server <- function(input, output) {}
-shinyApp(ui = ui, server = server)
+library(DT)   
+library(scales)
+library(ggridges)
+library(gridExtra)
+library(extrafont)
+
+
+olympics <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-27/olympics.csv')
+
 
 medal_colors <- c(
   Gold = "#d5a500",
   Silver = "#b7b7b7",
-  Bronze = "#a17419"
-)
+  Bronze = "#a17419")
+
+
+
+ui <- fluidPage("Olympic Medal Results")
+server <- function(input, output) {}
+shinyApp(ui = ui, server = server)
+
